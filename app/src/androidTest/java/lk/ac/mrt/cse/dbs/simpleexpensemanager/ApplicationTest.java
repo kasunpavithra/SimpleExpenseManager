f10ae327588f97527449b7ce88d7c4ff1104f42a
Kasun pavithra <mandalawattegekasunpavithra@gmail.com>
2022-05-11 18:07:10 +0530

    added test cases for all the funtions

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -24,10 +24,16 @@ import androidx.test.core.app.ApplicationProvider;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.List;
 
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistentExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -47,5 +53,76 @@ public class ApplicationTest {
         assertTrue(accountNumbers.contains("150A"));
 
     }
+    @Test
+    public void logTransactionTest(){
+        //add account first
+        String testAccountNO = "124B";
+        expenseManager.addAccount(testAccountNO,"ABC bank","Kasun",45000.00);
+        // log transaction and test
+        Date date = new Date();
+        TransactionDAO transactionDAO = expenseManager.getTransactionsDAO();
+        transactionDAO.logTransaction(date,testAccountNO, ExpenseType.INCOME,250.00);
+        Transaction transaction = new Transaction(date,testAccountNO,ExpenseType.INCOME,250.00);
+        List<Transaction> transactionsFromDatabase = transactionDAO.getPaginatedTransactionLogs(10);
+        assertTrue(transactionsFromDatabase.get(0).getAccountNo().equals(transaction.getAccountNo()));
+        assertTrue(transactionsFromDatabase.get(0).getAmount()==transaction.getAmount());
+        assertTrue(isSameDate(transaction.getDate(),transactionsFromDatabase.get(0).getDate()));
+
+    }
+
+    @Test
+    public void updateAccountBalanceTest1() {
+        String testAccountNO = "400C";
+        expenseManager.addAccount(testAccountNO,"ABC bank","Kasun",45000.00);
+
+        AccountDAO accountDAO = expenseManager.getAccountsDAO();
+        TransactionDAO transactionDAO = expenseManager.getTransactionsDAO();
+
+        try {
+            double previousBalance = accountDAO.getAccount(testAccountNO).getBalance();
+            expenseManager.updateAccountBalance(testAccountNO, 22, 5, 2022, ExpenseType.INCOME, "250.00");
+           assertTrue(accountDAO.getAccount(testAccountNO).getBalance()==previousBalance+250.00);
+        } catch (InvalidAccountException e) {
+            assertTrue(false);
+        }
+    }
+
+    @Test
+    public void updateAccountBalanceTest2() {
+        String testAccountNO = "777Z";
+        expenseManager.addAccount(testAccountNO,"ABC bank","Kasun",45000.00);
+
+        AccountDAO accountDAO = expenseManager.getAccountsDAO();
+        TransactionDAO transactionDAO = expenseManager.getTransactionsDAO();
+
+        try {
+            double previousBalance = accountDAO.getAccount(testAccountNO).getBalance();
+            expenseManager.updateAccountBalance(testAccountNO, 22, 5, 2022, ExpenseType.EXPENSE, "250.00");
+            assertTrue(accountDAO.getAccount(testAccountNO).getBalance()==previousBalance-250.00);
+        } catch (InvalidAccountException e) {
+            assertTrue(false);
+        }
+    }
+
+    @Test
+    public void removeAccountTest(){
+        String testAccountNO = "800P";
+        expenseManager.addAccount(testAccountNO,"ABC bank","Kasun",45000.00);
+
+        try {
+            expenseManager.getAccountsDAO().removeAccount(testAccountNO);
+            assertTrue(!expenseManager.getAccountNumbersList().contains(testAccountNO));
+        } catch (InvalidAccountException e) {
+            assertTrue(false);
+        }
+    }
+
+    private boolean isSameDate(Date date1,Date date2){
+        if(date1.getDate()==date2.getDate() && date1.getMonth()==date2.getMonth() && date1.getYear()==date2.getYear()){
+            return true;
+        }
+        return false;
+    }
+
 
 }
